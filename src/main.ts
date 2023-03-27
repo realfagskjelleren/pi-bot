a162f5d71040faaadc5e5b293435a370626f7edc
Tor Jacob Neple <torjne@stud.ntnu.no>
2023-03-27 17:21:03 +0200

    feat: get role from button

--- src/main.ts
+++ src/main.ts
@@ -2,46 +2,30 @@ import { dirname, importx } from "@discordx/importer";
 import type { Interaction, Message } from "discord.js";
 import { IntentsBitField } from "discord.js";
 import { Client } from "discordx";
+import * as dotenv from "dotenv"; dotenv.config();
 
 export const bot = new Client({
-  // To use only guild command
-  // botGuilds: [(client) => client.guilds.cache.map((guild) => guild.id)],
-
-  // Discord intents
+  // botGuilds: [(client) => client.guilds.cache.map((guild) => guild.id)], // Limit bot commands to specific guilds
   intents: [
     IntentsBitField.Flags.Guilds,
-    IntentsBitField.Flags.GuildMembers,
-    IntentsBitField.Flags.GuildMessages,
-    IntentsBitField.Flags.GuildMessageReactions,
-    IntentsBitField.Flags.GuildVoiceStates,
-    IntentsBitField.Flags.MessageContent,
+    // IntentsBitField.Flags.GuildMembers,
+    // IntentsBitField.Flags.GuildMessages,
+    // IntentsBitField.Flags.GuildMessageReactions,
+    // IntentsBitField.Flags.GuildVoiceStates,
+    // IntentsBitField.Flags.MessageContent,
   ],
-
-  // Debug logs are disabled in silent mode
-  silent: false,
-
-  // Configuration for @SimpleCommand
-  simpleCommand: {
+  silent: false, // If true, debug messages are not logged
+  simpleCommand: { // Config for @simpleCommand decorator
     prefix: "!",
   },
 });
 
 bot.once("ready", async () => {
-  // Make sure all guilds are cached
-  // await bot.guilds.fetch();
+  // await bot.guilds.fetch(); // Make sure all guilds are cached
+  await bot.initApplicationCommands(); // Sync all appcommands with Discord
 
-  // Synchronize applications commands with Discord
-  await bot.initApplicationCommands();
-
-  // To clear all guild commands, uncomment this line,
-  // This is useful when moving from guild commands to global commands
-  // It must only be executed once
-  //
-  //  await bot.clearApplicationCommands(
-  //    ...bot.guilds.cache.map((g) => g.id)
-  //  );
-
-  console.log("Bot started");
+  console.log("🤖 --< I AM... ALIVE? >");
+  console.log(`🤖 --< Logged in as ${bot.user?.tag} >`);
 });
 
 bot.on("interactionCreate", (interaction: Interaction) => {
@@ -52,21 +36,13 @@ bot.on("messageCreate", (message: Message) => {
   bot.executeCommand(message);
 });
 
-async function run() {
-  // The following syntax should be used in the commonjs environment
-  //
-  // await importx(__dirname + "/{events,commands}/**/*.{ts,js}");
+const run = async () => {
+  // await importx(__dirname + "/{events,commands}/**/*.{ts,js}"); // CommonJS
+  await importx(`${dirname(import.meta.url)}/{events,commands}/**/*.{ts,js}`); // ECMAScript
 
-  // The following syntax should be used in the ECMAScript environment
-  await importx(`${dirname(import.meta.url)}/{events,commands}/**/*.{ts,js}`);
+  if (!process.env.BOT_TOKEN) throw Error("No BOT_TOKEN in your env");
 
-  // Let's start the bot
-  if (!process.env.BOT_TOKEN) {
-    throw Error("Could not find BOT_TOKEN in your environment");
-  }
-
-  // Log in with your bot token
   await bot.login(process.env.BOT_TOKEN);
-}
+};
 
 run();
