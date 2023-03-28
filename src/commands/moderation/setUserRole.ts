5d90d754dd6cbfdd742b54da700b44a79a824945
Tor Jacob Neple <62819835+torjacob@users.noreply.github.com>
2023-03-28 12:32:48 +0200

    fix: make so user can't give role higher than own

--- src/commands/moderation/setUserRole.ts
+++ src/commands/moderation/setUserRole.ts
@@ -1,4 +1,4 @@
-import { ApplicationCommandOptionType, CommandInteraction, User } from 'discord.js';
+import { ApplicationCommandOptionType, CommandInteraction, GuildMember, Role, User } from 'discord.js';
 import { Discord, Slash, SlashOption } from 'discordx';
 
 @Discord()
@@ -16,14 +16,20 @@ export class SetUserRole {
             name: 'role',
             required: true,
             type: ApplicationCommandOptionType.Role,
-        }) role: string,
+        }) role: Role,
         interaction: CommandInteraction,
 	) {
         const member = interaction.guild?.members.cache.get(user.id);
+
         if (!member) {
             interaction.reply({ephemeral: true, content:`Couldn't find user ${user} on this server`});
             return;
         }
+        if (role.position >= (interaction.member as GuildMember).roles.highest.position ) {
+            interaction.reply({ephemeral: true, content:`You cannot give a role higher than your own`});
+            return;
+        }
+
         member.roles.add(role);
         interaction.reply({ephemeral: true, content:`Added role ${role} to ${user}`});
     }
