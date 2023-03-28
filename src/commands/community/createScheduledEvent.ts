aeedab5031f76c5d31492c327438f5876bdc370e
Tor Jacob Neple <tor.jacob.neple@gmail.com>
2023-03-28 02:35:05 +0200

    feat: create slashcommand to crate events

---
+++ src/commands/community/createScheduledEvent.ts
@@ -0,0 +1,113 @@
+import { sanitizeUrl } from '@braintree/sanitize-url';
+import type { GuildScheduledEvent } from 'discord.js';
+import {
+	ApplicationCommandOptionType,
+	CommandInteraction,
+	Guild,
+	GuildScheduledEventCreateOptions,
+	GuildScheduledEventEntityType,
+	GuildScheduledEventManager,
+	GuildScheduledEventPrivacyLevel,
+	GuildVoiceChannelResolvable,
+	InteractionReplyOptions
+} from 'discord.js';
+import { Discord, Slash, SlashChoice, SlashOption } from 'discordx';
+import { bot } from '../../main.js';
+
+@Discord()
+export class CreateScheduledEvent {
+	@Slash({
+		description: 'Create a scheduled event',
+		name: 'create-scheduled-event',
+	})
+	async execute(
+		@SlashOption({
+			description: 'Event name',
+			name: 'name',
+			required: true,
+			type: ApplicationCommandOptionType.String,
+		}) name: string,
+
+		@SlashOption({
+			description: 'Event description',
+			name: 'description',
+			required: true,
+			type: ApplicationCommandOptionType.String,
+		}) description: string,
+
+		@SlashOption({
+			description: 'Event start time',
+			name: 'start-time',
+			required: true,
+			type: ApplicationCommandOptionType.String,
+		}) startTime: string,
+
+		@SlashOption({
+			description: 'Event end time',
+			name: 'end-time',
+			required: true,
+			type: ApplicationCommandOptionType.String,
+		}) endTime: string,
+
+		@SlashChoice('Voice', 'External')
+		@SlashOption({
+			description: 'Event type',
+			name: 'event_type',
+			required: true,
+			type: ApplicationCommandOptionType.String,
+		}) event_type: string,
+
+		@SlashOption({
+			description: 'Event location (Optional, defaults to "Realfagskjelleren")',
+			name: 'location',
+			required: false,
+			type: ApplicationCommandOptionType.String,
+		}) location: string = 'Realfagskjelleren',
+
+		@SlashOption({
+			description: 'Url to event image (Optional, defaults to rfk logo)',
+			name: 'image-url',
+			required: false,
+			type: ApplicationCommandOptionType.String,
+		}) image: string = 'https://images.squarespace-cdn.com/content/v1/61eeb21558235b048d9ca47c/1665414460649-WT01WM3GU3255GX9J2VX/Lyskasse_oransj_svart.png?format=1500w',
+
+		@SlashOption({
+			description: 'Event Voice Channel Name (Optional, defaults to "General")',
+			name: 'channel_name',
+			required: false,
+			type: ApplicationCommandOptionType.String,
+		}) channel_name: string = 'General',
+
+		interaction: CommandInteraction
+	): Promise<void> {
+		const event_type_int: number = GuildScheduledEventEntityType[event_type as keyof typeof GuildScheduledEventEntityType];
+		await interaction.deferReply({ ephemeral: true });
+		const guild: Guild = bot.guilds.cache.get(
+			process.env.GUILD_ID as string
+		) as Guild;
+		const manager: GuildScheduledEventManager = guild.scheduledEvents;
+		const channel_id = guild.channels.cache.find(
+			(channel) => channel.name === channel_name
+		);
+		const options: GuildScheduledEventCreateOptions = {
+			name: name,
+			description: description,
+			scheduledStartTime: new Date(startTime),
+			scheduledEndTime: new Date(endTime),
+			image: sanitizeUrl(image),
+			privacyLevel: 2 as GuildScheduledEventPrivacyLevel,
+			entityType: event_type_int,
+			entityMetadata: { location: location },
+			channel: channel_id as GuildVoiceChannelResolvable,
+		};
+
+		manager.create(options).then((event: GuildScheduledEvent) => {
+			const replystring = `Event created: ${event.name}\nScheduled for: ${startTime} to ${endTime}\n${event_type_int == 3 ? 'Location: ' + location : 'Voidce Channel: ' + channel_name}`;
+
+			interaction.editReply({
+				ephemeral: true,
+				content: replystring,
+			} as InteractionReplyOptions);
+		});
+	}
+}
